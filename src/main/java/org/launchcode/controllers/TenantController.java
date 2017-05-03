1a91b75809efca00018d5da018463af3319053ed
Henry Lau <henrywlau@yahoo.com>
2017-05-03 13:32:06 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/controllers/TenantController.java
@@ -0,0 +1,10 @@
+package org.launchcode.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by henry on 5/3/17.
+ */
+@Controller
+public class TenantController {
+}
---
+++ src/main/java/org/launchcode/models/forms/Building.java
@@ -0,0 +1,7 @@
+package org.launchcode.models.forms;
+
+/**
+ * Created by henry on 5/3/17.
+ */
+public class Building {
+}
