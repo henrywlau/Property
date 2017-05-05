ea077a54a49c48772855e9106b09717d1ec94d20
Henry Lau <henrywlau@yahoo.com>
2017-05-05 00:20:51 -0500

    Added process remove Tenant form.

--- src/main/java/org/launchcode/controllers/TenantController.java
+++ src/main/java/org/launchcode/controllers/TenantController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 
@@ -55,6 +56,12 @@ public class TenantController {
         return "tenant/remove";
     }
 
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveTenantForm(@RequestParam int[] tenantIds) {
 
-
+        for (int tenantId : tenantIds) {
+            tenantDao.delete(tenantId);
+        }
+        return "redirect:";
+    }
 }
