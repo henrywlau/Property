afa3301723d5157a2a527f93dc7111f80eac1afb
Henry Lau <henrywlau@yahoo.com>
2017-05-04 23:55:17 -0500

    Added process Add Tenant form.

--- src/main/java/org/launchcode/controllers/TenantController.java
+++ src/main/java/org/launchcode/controllers/TenantController.java
@@ -5,8 +5,12 @@ import org.launchcode.models.forms.Tenant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by henry on 5/3/17.
@@ -32,5 +36,16 @@ public class TenantController {
         return "tenant/add";
     }
 
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddTenantForm(@ModelAttribute @Valid Tenant newTenant, Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Tenant");
+            return "tenant/add";
+        }
+
+        tenantDao.save(newTenant);
+        return "redirect:";
+    }
 
 }
