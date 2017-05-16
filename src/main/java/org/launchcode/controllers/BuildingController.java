a003bffa0609db016a326b2c73e8c8a055473195
Henry Lau <henrywlau@yahoo.com>
2017-05-16 00:42:59 -0500

    Completed process edit form on buildings.

--- src/main/java/org/launchcode/controllers/BuildingController.java
+++ src/main/java/org/launchcode/controllers/BuildingController.java
@@ -64,14 +64,14 @@ public class BuildingController {
 
     @RequestMapping(value = "edit/{buildingId}", method = RequestMethod.GET)
     public String displayEditForm(Model model, @PathVariable int buildingId) {
-        Building building = buildingDao.findOne(buildingId);
+        Building findBuilding = buildingDao.findOne(buildingId);
         model.addAttribute("title", "Edit Building");
-        model.addAttribute("building", building);
+        model.addAttribute("building", findBuilding);
         return "building/edit";
     }
 
     @RequestMapping(value = "edit/{buildingId}", method = RequestMethod.POST)
-    public String processEditForm(@Valid Building building, Errors errors, Model model, @PathVariable int buildingId, @RequestParam("address") String address, @RequestParam("units") Integer units, @RequestParam("city") String city, @RequestParam("state") String state, @RequestParam("zipCode") Integer zipCode) {
+    public String processEditForm(@ModelAttribute @Valid Building building, Errors errors, Model model, @PathVariable("buildingId") int buildingId) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Edit Building");
@@ -79,15 +79,38 @@ public class BuildingController {
             return "building/edit";
         }
 
-        building.setAddress(address);
-        building.setUnits(units);
-        building.setCity(city);
-        building.setState(state);
-        building.setZipCode(zipCode);
-        building.setId(buildingId);
-        buildingDao.save(building);
+        Building myBuilding = buildingDao.findOne(buildingId);
+        myBuilding.setId(building.getId());
+        myBuilding.setAddress(building.getAddress());
+        myBuilding.setUnits(building.getUnits());
+        myBuilding.setCity(building.getCity());
+        myBuilding.setState(building.getState());
+        myBuilding.setZipCode(building.getZipCode());
+
+        buildingDao.save(myBuilding);
         model.addAttribute("title", "Edit Building");
         model.addAttribute("successMessage", "Building has been modified!");
         return "building/edit";
     }
+
+//    @RequestMapping(value = "edit/{buildingId}", method = RequestMethod.POST)
+//    public String processEditForm(@ModelAttribute @Valid Building building, Errors errors, Model model, @PathVariable int buildingId, @RequestParam("address") String address, @RequestParam("units") Integer units, @RequestParam("city") String city, @RequestParam("state") String state, @RequestParam("zipCode") Integer zipCode) {
+//
+//        if (errors.hasErrors()) {
+//            model.addAttribute("title", "Edit Building");
+//            model.addAttribute("building", building);
+//            return "building/edit";
+//        }
+//
+//        building.setAddress(address);
+//        building.setUnits(units);
+//        building.setCity(city);
+//        building.setState(state);
+//        building.setZipCode(zipCode);
+//        building.setId(buildingId);
+//        buildingDao.save(building);
+//        model.addAttribute("title", "Edit Building");
+//        model.addAttribute("successMessage", "Building has been modified!");
+//        return "building/edit";
+//    }
 }
